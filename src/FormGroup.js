8645472b2327a882fb789b530cb4f0e9abed7ae5
CodeDraken <codedraken@users.noreply.github.com>
2018-09-16 14:23:56 -0500

    Add FormGroup component

---
+++ src/FormGroup.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+
+const propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string
+}
+
+const defaultProps = {
+  children: null,
+  className: ''
+}
+
+export const FormGroup = ({ children, ...props }) => {
+  const { className, ...attributes } = props
+  const classNames = classnames('form-group', className)
+
+  return (
+    <div {...attributes} className={classNames}>
+      { children }
+    </div>
+  )
+}
+
+FormGroup.propTypes = propTypes
+FormGroup.defaultProps = defaultProps
+
+export default FormGroup
