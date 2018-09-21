6aa6d597301b7cf62936608b13c004ae8938aa15
CodeDraken <codedraken@users.noreply.github.com>
2018-09-20 20:40:05 -0500

    fix tests typo

--- src/__test__/label.test.js
+++ src/__test__/label.test.js
@@ -5,55 +5,55 @@ import renderer from 'react-test-renderer'
 import { Label } from '../'
 
 describe('Label', () => {
-  it('should render a span tag by default', () => {
+  it('Should render a span tag by default', () => {
     const wrapper = shallow(<Label />)
 
     expect(wrapper.type()).toBe('span')
   })
 
-  it('should render children', () => {
+  it('Should render children', () => {
     const wrapper = shallow(<Label>My label</Label>)
 
     expect(wrapper.text()).toBe('My label')
   })
 
-  it('should pass additional classNames', () => {
+  it('Should pass additional classNames', () => {
     const wrapper = shallow(<Label className='extra'>My label</Label>)
 
     expect(wrapper.hasClass('extra')).toBe(true)
   })
 
-  it('should render with "label" class by default', () => {
+  it('Should render with "label" class by default', () => {
     const wrapper = shallow(<Label>Label</Label>)
 
     expect(wrapper.hasClass('label')).toBe(true)
   })
 
-  it('should render with "label-rounded" class when "rounded" is provided', () => {
+  it('Should render with "label-rounded" class when "rounded" is provided', () => {
     const wrapper = shallow(<Label rounded>Round</Label>)
 
     expect(wrapper.hasClass('label-rounded')).toBe(true)
   })
 
-  it('should render with "form-label" class when "form" is provided', () => {
+  it('Should render with "form-label" class when "form" is provided', () => {
     const wrapper = shallow(<Label form>form labels show above input</Label>)
 
     expect(wrapper.hasClass('form-label')).toBe(true)
   })
 
-  it('should render a label tag when its passed a "for" attribute', () => {
+  it('Should render a label tag when its passed a "for" attribute', () => {
     const wrapper = shallow(<Label for='test'>For attr</Label>)
 
     expect(wrapper.type()).toBe('label')
   })
 
-  it('should render with "label-{color}" class when a color is provided', () => {
+  it('Should render with "label-{color}" class when a color is provided', () => {
     const wrapper = shallow(<Label color='primary'>Primary</Label>)
 
     expect(wrapper.hasClass('label-primary')).toBe(true)
   })
 
-  it('should render custom tag', () => {
+  it('Should render custom tag', () => {
     const wrapper = shallow(<Label renderAs='main'>custom</Label>)
 
     expect(wrapper.type()).toBe('main')
