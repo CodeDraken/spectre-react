263933abdece761f3e2c81da6d0183783fbb2318
CodeDraken <codedraken@users.noreply.github.com>
2018-10-14 19:20:52 -0500

    Add Icon tests

--- src/Icon.js
+++ src/Icon.js
@@ -4,24 +4,27 @@ import classnames from 'classnames'
 
 const propTypes = {
   className: PropTypes.string,
-  icon: PropTypes.string,
+  icon: PropTypes.string.isRequired,
   size: PropTypes.oneOf(['2x', '3x', '4x']),
-  formIcon: PropTypes.bool
+  formIcon: PropTypes.bool,
+  renderAs: PropTypes.oneOfType([ PropTypes.string, PropTypes.func ])
 }
 
 const defaultProps = {
   className: null,
   icon: null,
   size: null,
-  formIcon: false
+  formIcon: false,
+  renderAs: 'i'
 }
 
 export const Icon = ({ children, ...props }) => {
-  const { className, formIcon, icon, size, ...attributes } = props
+  const { className, formIcon, icon, size, renderAs: Element, ...attributes } = props
 
   const classNames = classnames(
     'icon',
     `icon-${icon}`,
+    className,
     {
       [`icon-${size}`]: size,
       'form-icon': formIcon
@@ -29,7 +32,7 @@ export const Icon = ({ children, ...props }) => {
   )
 
   return (
-    <i {...attributes} className={classNames} />
+    <Element {...attributes} className={classNames} />
   )
 }
 
