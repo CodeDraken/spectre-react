4eb2a7b1ac8169e661801791bc1503909211abb1
CodeDraken <codedraken@users.noreply.github.com>
2018-10-28 23:28:47 -0500

    Add base AccordionHeader component

---
+++ src/Accordion/AccordionHeader.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+
+const propTypes = {
+  children: PropTypes.node,
+  renderAs: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  className: PropTypes.string
+}
+
+const defaultProps = {
+  renderAs: 'label'
+}
+
+const AccordionHeader = ({ children, ...props }) => {
+  const { className, renderAs: Element, ...attributes } = props
+  const classNames = classnames('accordion-header', className)
+
+  return (
+    <Element {...attributes} className={classNames}>
+      { children }
+    </Element>
+  )
+}
+
+AccordionHeader.propTypes = propTypes
+AccordionHeader.defaultProps = defaultProps
+
+export default AccordionHeader
