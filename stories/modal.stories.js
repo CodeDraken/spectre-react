0776ebfe77912c42df8539d76d9b133e7b19f5fa
CodeDraken <codedraken@users.noreply.github.com>
2018-10-30 18:24:43 -0500

    Update modal story

--- stories/modal.stories.js
+++ stories/modal.stories.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { boolean } from '@storybook/addon-knobs'
 
 import { Modal, Grid, Button, ModalTitle } from '../src'
 
